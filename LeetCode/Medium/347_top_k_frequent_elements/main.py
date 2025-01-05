bdc5262c5d49358ab23ca47c15aaafe1310991a1
Nate Stott <nate.stott@pm.me>
2025-01-05 17:14:54 -0500

    do 347

---
+++ LeetCode/Medium/347_top_k_frequent_elements/main.py
@@ -0,0 +1,16 @@
+import sys
+from typing import List
+
+from solution import Solution
+
+
+def main(args: List[str]):
+    if len(args) < 3:
+        print("Usage: python main.py <nums> <k>")
+        return
+    nums: List[int] = [int(i) for i in args[1:len(args)-1]]
+    k: int = int(args[len(args)-1])
+    print(Solution().topKFrequent(nums, k))
+
+if __name__ == "__main__":
+    main(sys.argv)
---
+++ LeetCode/Medium/347_top_k_frequent_elements/solution.py
@@ -0,0 +1,12 @@
+from typing import List, Dict
+
+
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        m: Dict[int, int] = {}
+        for n in nums:
+            m[n] = m.get(n, 0) + 1
+
+        s = sorted(m.items(), key=lambda x: x[1], reverse=True)
+
+        return [i[0] for i in s[:k]]
