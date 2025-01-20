dd4aa5088942f9803e9ee36b3506bccb8f9130c1
Nate Stott <nate.stott@pm.me>
2025-01-20 16:48:39 -0500

    do 84

---
+++ LeetCode/Hard/84_largest_rectangle_in_histogram/test.py
@@ -0,0 +1,18 @@
+import unittest
+
+from solution import Solution
+
+
+class TestAddFunction(unittest.TestCase):
+
+    def test_1(self):
+        result = Solution().largestRectangleArea([2,1,5,6,2,3])
+        self.assertEqual(result, 10)
+
+    def test_2(self):
+        result = Solution().largestRectangleArea([2,4])
+        self.assertEqual(result, 4)
+
+
+if __name__ == "__main__":
+    unittest.main()
