077aea227c94b01a863a0919abe29cddce4ab517
Nate Stott <nate.stott@pm.me>
2025-01-22 21:07:42 -0500

    do 15

---
+++ LeetCode/Medium/15_3sum/try2/main.py
@@ -0,0 +1,16 @@
+import sys
+from typing import List
+
+from solution import Solution
+
+
+def main(args: List[str]):
+    if len(args) < 2:
+        print("Usage: python main.py <n>")
+        return
+    nums: List[int] = [int(i) for i in args[1:]]
+    print(Solution().threeSum(nums))
+
+
+if __name__ == "__main__":
+    main(sys.argv)
