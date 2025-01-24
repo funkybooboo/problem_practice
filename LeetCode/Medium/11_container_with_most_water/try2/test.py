c601117a9f8402405bb209a404bbc81d7734627f
Nate Stott <nate.stott@pm.me>
2025-01-23 20:43:08 -0500

    do 11

---
+++ LeetCode/Medium/11_container_with_most_water/try2/test.py
@@ -0,0 +1,18 @@
+import unittest
+
+from solution import Solution
+
+
+class TestAddFunction(unittest.TestCase):
+
+    def test_1(self):
+        result = Solution().maxArea([1,8,6,2,5,4,8,3,7])
+        self.assertEqual(result, 49)
+
+    def test_2(self):
+        result = Solution().maxArea([1,1])
+        self.assertEqual(result, 1)
+
+
+if __name__ == "__main__":
+    unittest.main()
