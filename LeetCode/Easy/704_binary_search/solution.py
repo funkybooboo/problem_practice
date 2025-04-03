84042672e94dd87c5c8bc79cdfbe5434763e0c34
Nate Stott <nate.stott@pm.me>
2025-04-02 21:59:39 -0400

    704

---
+++ LeetCode/Easy/704_binary_search/solution.py
@@ -0,0 +1,19 @@
+from typing import List
+
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        return self._search_helper(nums, target, 0, len(nums) - 1)
+
+    def _search_helper(self, nums: List[int], target: int, left: int, right: int) -> int:
+        if left > right:
+            return -1
+
+        mid = (left + right) // 2
+
+        if nums[mid] == target:
+            return mid
+        elif nums[mid] < target:
+            return self._search_helper(nums, target, mid + 1, right)
+        else:
+            return self._search_helper(nums, target, left, mid - 1)
