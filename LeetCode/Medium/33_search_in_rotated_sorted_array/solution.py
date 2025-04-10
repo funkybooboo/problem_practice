bf79a7b5df8649a0f9a8fb47153c45d9def6fb28
Nate Stott <nate.stott@pm.me>
2025-04-09 22:14:45 -0400

    do 33

---
+++ LeetCode/Medium/33_search_in_rotated_sorted_array/solution.py
@@ -0,0 +1,49 @@
+from typing import List
+
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        if len(nums) == 0:
+            return -1
+        if len(nums) == 1:
+            return 0 if target == nums[0] else -1
+        left = 0
+        right = len(nums) - 1
+        if nums[left] == target:
+            return left
+        if nums[right] == target:
+            return right
+        if nums[left] < nums[right]:
+            return self._b_search(nums, target)
+
+        while left <= right:
+            mid = (left + right) // 2
+            if nums[mid] == target:
+                return mid
+            if nums[left] <= nums[mid]:
+                if nums[left] > target or nums[mid] < target:
+                    left = mid + 1
+                else:
+                    right = mid - 1
+            else:
+                if nums[mid] > target or nums[right] < target:
+                    right = mid - 1
+                else:
+                    left = mid + 1
+        return -1
+
+    def _b_search(self, nums: List[int], target: int) -> int:
+        return self._search_helper(nums, target, 0, len(nums) - 1)
+
+    def _search_helper(self, nums: List[int], target: int, left: int, right: int) -> int:
+        if left > right:
+            return -1
+
+        mid = (left + right) // 2
+
+        if nums[mid] == target:
+            return mid
+        elif nums[mid] < target:
+            return self._search_helper(nums, target, mid + 1, right)
+        else:
+            return self._search_helper(nums, target, left, mid - 1)
---
+++ LeetCode/Medium/33_search_in_rotated_sorted_array/test.py
@@ -0,0 +1,29 @@
+import unittest
+
+from solution import Solution
+
+
+class TestAddFunction(unittest.TestCase):
+
+    def test_1(self):
+        result = Solution().search([4,5,6,7,0,1,2], 0)
+        self.assertEqual(result, 4)
+
+    def test_2(self):
+        result = Solution().search([4,5,6,7,0,1,2], 3)
+        self.assertEqual(result, -1)
+
+    def test_3(self):
+        result = Solution().search([1], 0)
+        self.assertEqual(result, -1)
+
+    def test_4(self):
+        result = Solution().search([3,4,5,6,1,2], 1)
+        self.assertEqual(result, 4)
+
+    def test_5(self):
+        result = Solution().search([3,5,6,0,1,2], 4)
+        self.assertEqual(result, -1)
+
+if __name__ == "__main__":
+    unittest.main()
