96209d998460dae471ff660e28d58218e7c86a9c
Nate Stott <nate.stott@pm.me>
2025-04-14 17:50:13 -0400

    21

---
+++ LeetCode/Easy/21_merge_two_sorted_lists/try2/solution.py
@@ -0,0 +1,46 @@
+from typing import Optional, List
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+    @staticmethod
+    def from_list(items: List[int]) -> Optional['ListNode']:
+        dummy = ListNode()
+        current = dummy
+        for item in items:
+            current.next = ListNode(item)
+            current = current.next
+        return dummy.next
+
+    @staticmethod
+    def to_list(node: Optional['ListNode']) -> List[int]:
+        result = []
+        while node:
+            result.append(node.val)
+            node = node.next
+        return result
+
+
+class Solution:
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        if not list1:
+            return list2
+        if not list2:
+            return list1
+
+        dummy = ListNode(-1)
+        current = dummy
+
+        while list1 and list2:
+            if list1.val < list2.val:
+                current.next = list1
+                list1 = list1.next
+            else:
+                current.next = list2
+                list2 = list2.next
+            current = current.next
+
+        current.next = list1 if list1 else list2
+        return dummy.next
---
+++ LeetCode/Easy/21_merge_two_sorted_lists/try2/test.py
@@ -0,0 +1,37 @@
+import unittest
+from solution import ListNode, Solution
+
+class TestMergeTwoLists(unittest.TestCase):
+
+    def test_1(self):
+        list1 = ListNode.from_list([1, 2, 4])
+        list2 = ListNode.from_list([1, 3, 4])
+        result = Solution.mergeTwoLists(list1, list2)
+        self.assertEqual(ListNode.to_list(result), [1, 1, 2, 3, 4, 4])
+
+    def test_2(self):
+        list1 = ListNode.from_list([])
+        list2 = ListNode.from_list([])
+        result = Solution.mergeTwoLists(list1, list2)
+        self.assertEqual(ListNode.to_list(result), [])
+
+    def test_3(self):
+        list1 = ListNode.from_list([])
+        list2 = ListNode.from_list([0])
+        result = Solution.mergeTwoLists(list1, list2)
+        self.assertEqual(ListNode.to_list(result), [0])
+
+    def test_4(self):
+        list1 = ListNode.from_list([1, 2, 4])
+        list2 = ListNode.from_list([1, 3, 5])
+        result = Solution.mergeTwoLists(list1, list2)
+        self.assertEqual(ListNode.to_list(result), [1, 1, 2, 3, 4, 5])
+
+    def test_5(self):
+        list1 = ListNode.from_list([])
+        list2 = ListNode.from_list([1, 2])
+        result = Solution.mergeTwoLists(list1, list2)
+        self.assertEqual(ListNode.to_list(result), [1, 2])
+
+if __name__ == "__main__":
+    unittest.main()
