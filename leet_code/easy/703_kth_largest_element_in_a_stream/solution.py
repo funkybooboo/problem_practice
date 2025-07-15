19b2e916c2068b54598b5a4e0c289b1e9d8dbe7b
Nate Stott <nate.stott@pm.me>
2025-07-14 20:07:30 -0400

    do 703

---
+++ leet_code/easy/703_kth_largest_element_in_a_stream/solution.py
@@ -0,0 +1,18 @@
+from typing import List
+import heapq
+
+class KthLargest:
+
+    def __init__(self, k: int, nums: List[int]):
+        self._k = k
+        self._heap = nums
+        heapq.heapify(self._heap)
+        while len(self._heap) > k:
+            heapq.heappop(self._heap)  # keep only k largest
+
+    def add(self, val: int) -> int:
+        if len(self._heap) < self._k:
+            heapq.heappush(self._heap, val)
+        elif val > self._heap[0]:
+            heapq.heapreplace(self._heap, val)  # faster than heappop+heappush
+        return self._heap[0]
---
+++ leet_code/easy/703_kth_largest_element_in_a_stream/test.py
@@ -0,0 +1,34 @@
+import unittest
+from solution import KthLargest
+
+class TestKthLargest(unittest.TestCase):
+
+    def test_example_1(self):
+        kth = KthLargest(3, [4, 5, 8, 2])
+        self.assertEqual(kth.add(3), 4)
+        self.assertEqual(kth.add(5), 5)
+        self.assertEqual(kth.add(10), 5)
+        self.assertEqual(kth.add(9), 8)
+        self.assertEqual(kth.add(4), 8)
+
+    def test_example_2(self):
+        kth = KthLargest(4, [7, 7, 7, 7, 8, 3])
+        self.assertEqual(kth.add(2), 7)
+        self.assertEqual(kth.add(10), 7)
+        self.assertEqual(kth.add(9), 7)
+        self.assertEqual(kth.add(9), 8)
+
+    def test_empty_init(self):
+        kth = KthLargest(1, [])
+        self.assertEqual(kth.add(5), 5)
+        self.assertEqual(kth.add(2), 5)
+        self.assertEqual(kth.add(10), 10)
+
+    def test_negative_values(self):
+        kth = KthLargest(2, [-10, -20, -30])
+        self.assertEqual(kth.add(-5), -10)
+        self.assertEqual(kth.add(-25), -10)
+        self.assertEqual(kth.add(-1), -5)
+
+if __name__ == "__main__":
+    unittest.main()
