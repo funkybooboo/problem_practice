4828598c0b06d0be10bd44ea969f38c90a5572b6
Nate Stott <nate.stott@pm.me>
2025-07-20 16:30:17 -0400

    do 355

---
+++ leet_code/medium/355_design_twitter/solution.py
@@ -0,0 +1,46 @@
+import heapq
+from collections import defaultdict
+from typing import List
+
+class Twitter:
+    def __init__(self):
+        self.timestamp = 0  # Decreasing timestamp for recency
+        self.user_tweets = defaultdict(list)   # userId -> list of [timestamp, tweetId]
+        self.user_follows = defaultdict(set)   # userId -> set of followeeIds
+
+    def postTweet(self, userId: int, tweetId: int) -> None:
+        # Store tweet with a timestamp (using negative for max-heap behavior)
+        self.user_tweets[userId].append([self.timestamp, tweetId])
+        self.timestamp -= 1
+
+    def getNewsFeed(self, userId: int) -> List[int]:
+        feed = []
+        heap = []
+
+        # Ensure the user sees their own tweets
+        self.user_follows[userId].add(userId)
+
+        for followeeId in self.user_follows[userId]:
+            tweets = self.user_tweets[followeeId]
+            if tweets:
+                index = len(tweets) - 1
+                time, tweetId = tweets[index]
+                # Push latest tweet of each followee onto the heap
+                heapq.heappush(heap, [time, tweetId, followeeId, index - 1])
+
+        while heap and len(feed) < 10:
+            time, tweetId, user, idx = heapq.heappop(heap)
+            feed.append(tweetId)
+            if idx >= 0:
+                next_time, next_tweetId = self.user_tweets[user][idx]
+                heapq.heappush(heap, [next_time, next_tweetId, user, idx - 1])
+
+        return feed
+
+    def follow(self, followerId: int, followeeId: int) -> None:
+        if followerId != followeeId:
+            self.user_follows[followerId].add(followeeId)
+
+    def unfollow(self, followerId: int, followeeId: int) -> None:
+        if followeeId in self.user_follows[followerId] and followerId != followeeId:
+            self.user_follows[followerId].remove(followeeId)
---
+++ leet_code/medium/355_design_twitter/test.py
@@ -0,0 +1,57 @@
+import unittest
+from solution import Twitter
+
+class TestTwitter(unittest.TestCase):
+
+    def test_example_scenario(self):
+        twitter = Twitter()
+        twitter.postTweet(1, 5)                   # User 1 posts tweet 5
+        self.assertEqual(twitter.getNewsFeed(1), [5])
+
+        twitter.follow(1, 2)                      # User 1 follows user 2
+        twitter.postTweet(2, 6)                   # User 2 posts tweet 6
+        self.assertEqual(twitter.getNewsFeed(1), [6, 5])
+
+        twitter.unfollow(1, 2)                    # User 1 unfollows user 2
+        self.assertEqual(twitter.getNewsFeed(1), [5])
+
+    def test_multiple_tweets_and_follows(self):
+        twitter = Twitter()
+        twitter.postTweet(1, 101)
+        twitter.postTweet(2, 102)
+        twitter.postTweet(1, 103)
+        twitter.postTweet(3, 104)
+        twitter.postTweet(2, 105)
+        twitter.follow(1, 2)
+        twitter.follow(1, 3)
+
+        news_feed = twitter.getNewsFeed(1)
+        self.assertEqual(news_feed, [105, 104, 103, 102, 101])
+
+    def test_limit_news_feed_to_10(self):
+        twitter = Twitter()
+        for i in range(15):
+            twitter.postTweet(1, i)
+        self.assertEqual(twitter.getNewsFeed(1), list(range(14, 4, -1)))
+
+    def test_unfollow_self_no_effect(self):
+        twitter = Twitter()
+        twitter.postTweet(1, 42)
+        twitter.unfollow(1, 1)  # Should not cause error or remove tweets
+        self.assertEqual(twitter.getNewsFeed(1), [42])
+
+    def test_follow_idempotency(self):
+        twitter = Twitter()
+        twitter.follow(1, 2)
+        twitter.follow(1, 2)
+        twitter.postTweet(2, 99)
+        self.assertEqual(twitter.getNewsFeed(1), [99])
+
+    def test_unfollow_non_followed(self):
+        twitter = Twitter()
+        twitter.postTweet(2, 88)
+        twitter.unfollow(1, 2)  # No-op
+        self.assertEqual(twitter.getNewsFeed(1), [])
+
+if __name__ == "__main__":
+    unittest.main()
