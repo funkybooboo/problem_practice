45cee689da7c09fb0c69873a8a8f68ef4c684249
Nate Stott <nate.stott@pm.me>
2025-09-19 06:36:26 -0600

    do 191

---
+++ leet_code/easy/191_number_of_1_bits/test.py
@@ -0,0 +1,25 @@
+import unittest
+from solution import Solution
+
+class TestHammingWeight(unittest.TestCase):
+    def setUp(self):
+        self.solution = Solution()
+
+    def test_example_1(self):
+        self.assertEqual(self.solution.hammingWeight(11), 3)
+
+    def test_example_2(self):
+        self.assertEqual(self.solution.hammingWeight(128), 1)
+
+    def test_example_3(self):
+        self.assertEqual(self.solution.hammingWeight(2147483645), 30)
+
+    def test_example_4(self):
+        self.assertEqual(self.solution.hammingWeight(int('00000000000000000000000000010111', 2)), 4)
+
+    def test_example_5(self):
+        self.assertEqual(self.solution.hammingWeight(int('01111111111111111111111111111101', 2)), 30)
+
+if __name__ == "__main__":
+    unittest.main()
+
