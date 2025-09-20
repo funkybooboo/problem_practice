e069c921d740bf18f72d3ef0a89e5f61aac5099d
Nate Stott <nate.stott@pm.me>
2025-09-20 07:24:16 -0600

    do 338

---
+++ leet_code/easy/338_counting_bits/solution.py
@@ -0,0 +1,11 @@
+from typing import List
+
+class Solution:
+    def countBits(self, n: int) -> List[int]:
+        counts = [0] * (n + 1)
+
+        for i in range(1, n + 1):
+            counts[i] = counts[i // 2] + (i % 2)
+
+        return counts
+
---
+++ leet_code/easy/338_counting_bits/test.py
@@ -0,0 +1,19 @@
+import unittest
+from solution import Solution
+
+class TestSolution(unittest.TestCase):
+
+    def test_example_1(self):
+        sol = Solution()
+        self.assertEqual(sol.countBits(2), [0, 1, 1])
+
+    def test_example_2(self):
+        sol = Solution()
+        self.assertEqual(sol.countBits(5), [0, 1, 1, 2, 1, 2])
+
+    def test_example_3(self):
+        sol = Solution()
+        self.assertEqual(sol.countBits(4), [0, 1, 1, 2, 1])
+
+if __name__ == "__main__":
+    unittest.main()
