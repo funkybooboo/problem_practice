9511377ef1cf55ab1e1d730d323c8865e0a2e1d6
Nate Stott <nate.stott@pm.me>
2025-10-09 21:23:26 -0600

    do 1267

---
+++ leet_code/medium/1267_count_servers_that_communicate/test.py
@@ -0,0 +1,41 @@
+import unittest
+
+from solution import Solution
+
+
+class TestSolution(unittest.TestCase):
+    def test_example_1(self):
+        solution = Solution()
+        grid = [[1,0],[0,1]]
+        self.assertEqual(solution.countServers(grid), 0)
+
+    def test_example_2(self):
+        solution = Solution()
+        grid = [[1,0],[1,1]]
+        self.assertEqual(solution.countServers(grid), 3)
+
+    def test_example_3(self):
+        solution = Solution()
+        grid = [[1,1,0,0],[0,0,1,0],[0,0,1,0],[0,0,0,1]]
+        self.assertEqual(solution.countServers(grid), 4)
+
+    def test_example_5(self):
+        solution = Solution()
+        grid = [
+            [1,1,0,0],
+            [0,0,1,0],
+            [0,0,1,0],
+            [0,0,0,1]
+        ]
+        self.assertEqual(solution.countServers(grid), 4)
+
+    def test_example_6(self):
+        solution = Solution()
+        grid = [
+            [1,0],
+            [1,1]
+        ]
+        self.assertEqual(solution.countServers(grid), 3)
+
+if __name__ == '__main__':
+    unittest.main()
