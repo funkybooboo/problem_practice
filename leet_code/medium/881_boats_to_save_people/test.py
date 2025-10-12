03c8e5e9f4fe4c9d56ea72ae390ef7433652c332
Nate Stott <nate.stott@pm.me>
2025-10-12 10:04:16 -0600

    do 881

---
+++ leet_code/medium/881_boats_to_save_people/test.py
@@ -0,0 +1,41 @@
+import unittest
+from solution import Solution
+
+class TestSolution(unittest.TestCase):
+    def test_example_1(self):
+        solution = Solution()
+        people = [1, 2]
+        limit = 3
+        expected_output = 1
+        self.assertEqual(solution.numRescueBoats(people, limit), expected_output)
+
+    def test_example_2(self):
+        solution = Solution()
+        people = [3, 2, 2, 1]
+        limit = 3
+        expected_output = 3
+        self.assertEqual(solution.numRescueBoats(people, limit), expected_output)
+
+    def test_example_3(self):
+        solution = Solution()
+        people = [3, 5, 3, 4]
+        limit = 5
+        expected_output = 4
+        self.assertEqual(solution.numRescueBoats(people, limit), expected_output)
+
+    def test_example_4(self):
+        solution = Solution()
+        people = [5, 1, 4, 2]
+        limit = 6
+        expected_output = 2
+        self.assertEqual(solution.numRescueBoats(people, limit), expected_output)
+
+    def test_example_5(self):
+        solution = Solution()
+        people = [1, 3, 2, 3, 2]
+        limit = 3
+        expected_output = 4
+        self.assertEqual(solution.numRescueBoats(people, limit), expected_output)
+
+if __name__ == '__main__':
+    unittest.main()
