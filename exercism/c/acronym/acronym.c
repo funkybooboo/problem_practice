ac286149e0fc9f41504e21679752154b7153bd5a
nate <nate.stott@pm.me>
2026-08-23 15:39:07 -0600

    c acronym

--- exercism/c/acronym/acronym.c
+++ exercism/c/acronym/acronym.c
@@ -1,1 +1,22 @@
 #include "acronym.h"
+#include <string.h>
+#include <ctype.h>
+#include <stdbool.h>
+
+char *abbreviate(const char *phrase) {
+    char *abbereviation = "";
+    size_t phrase_len = strlen(phrase);
+    bool prev_is_space = false;
+    char space = ' ';
+    for (size_t i = 0; i < phrase_len; i++) {
+        if (phrase[i] == space || (i == 0 && phrase[i] != space)) {
+            prev_is_space = true;
+            continue;
+        }
+        if (prev_is_space) {
+            abbereviation += toupper(phrase[i]);
+        }
+        prev_is_space = false;
+    }
+    return abbereviation;
+}
